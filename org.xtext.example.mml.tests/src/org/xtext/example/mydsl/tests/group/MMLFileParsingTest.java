ed8620e3aba9884ad2c7a97aceac9211e56f67e8
Abbah <anohabbah@gmail.com>
2020-02-17 21:02:17 +0100

    hvhbjbk

--- org.xtext.example.mml.tests/src/org/xtext/example/mydsl/tests/group/MMLFileParsingTest.java
+++ org.xtext.example.mml.tests/src/org/xtext/example/mydsl/tests/group/MMLFileParsingTest.java
@@ -1,32 +1,20 @@
 package org.xtext.example.mydsl.tests.group;
 
 import java.io.InputStream;
-import java.util.Objects;
 import java.util.Scanner;
 
-import javax.swing.Box.Filler;
-
-import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.xtext.testing.InjectWith;
 import org.eclipse.xtext.testing.extensions.InjectionExtension;
 import org.eclipse.xtext.testing.util.ParseHelper;
-import org.eclipse.xtext.testing.util.ResourceHelper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.xtext.example.mydsl.mml.CSVParsingConfiguration;
-import org.xtext.example.mydsl.mml.DT;
-import org.xtext.example.mydsl.mml.DataInput;
 import org.xtext.example.mydsl.mml.FrameworkLang;
-import org.xtext.example.mydsl.mml.LogisticRegression;
 import org.xtext.example.mydsl.mml.MLAlgorithm;
 import org.xtext.example.mydsl.mml.MLChoiceAlgorithm;
 import org.xtext.example.mydsl.mml.MMLModel;
-import org.xtext.example.mydsl.mml.RandomForest;
-import org.xtext.example.mydsl.mml.SVM;
 import org.xtext.example.mydsl.tests.MmlInjectorProvider;
 import org.xtext.example.mydsl.tests.group.compilers.SciKitCompiler;
 
-import com.google.common.io.Files;
 import com.google.inject.Inject;
 
 @ExtendWith(InjectionExtension.class)
@@ -48,8 +36,6 @@ public class MMLFileParsingTest {
 	}
 
 	private void algorithmFactory(MLChoiceAlgorithm choiceAlgorithm, MMLModel model, String filename) {
-		StringBuffer imports = new StringBuffer();
-
 		FrameworkLang framework = choiceAlgorithm.getFramework();
 		MLAlgorithm algorithm = choiceAlgorithm.getAlgorithm();
 
@@ -89,18 +75,5 @@ public class MMLFileParsingTest {
 
 	private String normalizeFilename(String filename) {
 		return filename.trim().replace(".mml", "");
-	}
-
-	private String pythonReadDataset(DataInput input) {
-		String location = input.getFilelocation();
-		String csvSep;
-		CSVParsingConfiguration parsingConfig = input.getParsingInstruction();
-		if (Objects.isNull(parsingConfig)) {
-			csvSep = ",";
-		} else {
-			csvSep = parsingConfig.getSep().toString();
-		}
-
-		return "df = pd.read_csv(\"" + location + "\", sep=\"" + csvSep + "\")";
 	}
 }
--- org.xtext.example.mml.tests/src/org/xtext/example/mydsl/tests/group/MmlParsingJavaSkLearn.java
+++ org.xtext.example.mml.tests/src/org/xtext/example/mydsl/tests/group/MmlParsingJavaSkLearn.java
@@ -26,31 +26,31 @@ public class MmlParsingJavaSkLearn {
 
 	@Inject
 	ParseHelper<MMLModel> parseHelper;
-	
+
 	private Map<String, String> variablesTable = new HashMap<>();
 
 	@Test
 	public void loadModel() throws Exception {
-		/*MMLModel result = parseHelper.parse("datainput \"foo.csv\"\n" + "mlframework scikit-learn\n" + "algorithm DT\n"
-				+ "TrainingTest { percentageTraining 70 }\n" + "recall\n" + "");*/
-		
-		MMLModel result = parseHelper.parse("datainput \"iris.csv\"separator;\r\n" + 
-				"mlframework scikit-learn\r\n" + 
-				"algorithm DecisionTree\r\n" + 
-				"formula \"colName\"+1+\"colName\"\r\n" + 
-				"TrainingTest{percentageTraining 70 }\r\n" + 
-				"accuracy" + "");
-		
+		/*
+		 * MMLModel result = parseHelper.parse("datainput \"foo.csv\"\n" +
+		 * "mlframework scikit-learn\n" + "algorithm DT\n" +
+		 * "TrainingTest { percentageTraining 70 }\n" + "recall\n" + "");
+		 */
+
+		MMLModel result = parseHelper.parse("datainput \"iris.csv\"separator;\r\n" + "mlframework scikit-learn\r\n"
+				+ "algorithm DecisionTree\r\n" + "formula \"colName\"+1+\"colName\"\r\n"
+				+ "TrainingTest{percentageTraining 70 }\r\n" + "accuracy" + "");
+
 		RFormula formul = result.getFormula();
-		//Assertions.assertEquals(formul.getPredictive().getColName(), "variety");
+		// Assertions.assertEquals(formul.getPredictive().getColName(), "variety");
 		Assertions.assertNull(formul.getPredictive());
 		Assertions.assertFalse(formul.getPredictors() instanceof AllVariables);
 		Assertions.assertTrue(formul.getPredictors() instanceof PredictorVariables);
-		
+
 		EList<FormulaItem> predictors = ((PredictorVariables) formul.getPredictors()).getVars();// liste des predictives
-		
+
 		Assertions.assertEquals(predictors.size(), 3);
-		
+
 		Assertions.assertNotNull(result);
 		EList<Resource.Diagnostic> errors = result.eResource().getErrors();
 		Assertions.assertTrue(errors.isEmpty(), "Unexpected errors");
@@ -77,18 +77,19 @@ public class MmlParsingJavaSkLearn {
 		// Import de bibliotheque python pandas pour gerer l'importation de fichier
 		String pythonImport = "import pandas as pd\n";
 		importTexte += pythonImport;
-		importTexte += genImportPackageCode(frameworkEnTraitement.getAlgorithm(),result.getFormula(), result.getValidation());
-		
-		body+= "\n"+genDataInputTraitement(result.getInput());
-		
-		body+= "\n"+genBodypartForPredictiveRFormula(result.getFormula());
-		
-		body += "\n"+genBodypartForAlgorithm(frameworkEnTraitement.getAlgorithm());
-		
-		body += "\n"+genBodypartForValidation(result.getValidation());
-		
+		importTexte += genImportPackageCode(frameworkEnTraitement.getAlgorithm(), result.getFormula(),
+				result.getValidation());
+
+		body += "\n" + genDataInputTraitement(result.getInput());
+
+		body += "\n" + genBodypartForPredictiveRFormula(result.getFormula());
+
+		body += "\n" + genBodypartForAlgorithm(frameworkEnTraitement.getAlgorithm());
+
+		body += "\n" + genBodypartForValidation(result.getValidation(), frameworkEnTraitement.getAlgorithm(), result);
+
 		codeFinalTexte = importTexte + body;
-		
+
 		Files.write(codeFinalTexte.getBytes(), new File("mml.py"));
 		// end of Python generation
 		/*
@@ -108,9 +109,9 @@ public class MmlParsingJavaSkLearn {
 	private String mkValueInSingleQuote(String val) {
 		return "'" + val + "'";
 	}
-	
+
 	private String genDataInputTraitement(DataInput dataInput) {
-		String datainputpart= "";
+		String datainputpart = "";
 		// Recuperation de l'objet dataInput Possedant les infos sur fichier et les
 		// instructions de parsingCSV
 		String fileLocation = dataInput.getFilelocation();
@@ -146,20 +147,18 @@ public class MmlParsingJavaSkLearn {
 		// Import pour la formula (Pour R uniquement)
 		String importFormula = "";
 		FormulaItem predictive = formula.getPredictive();
-		
+
 		XFormula predictors = formula.getPredictors();
 
 		// Import pour la validation
 		String importValidate = "";
-		if(validation instanceof CrossValidation) {
+		if (validation instanceof CrossValidation) {
 			importValidate = "from sklearn.model_selection import cross_validate\n";
-		}
-		else if(validation instanceof TrainingTest) {
+		} else if (validation instanceof TrainingTest) {
 			importValidate = "from sklearn.model_selection import train_test_split\n";
 		}
-		
+
 		importValidate += "from sklearn import metrics\n";
-		
 
 		// Import Code (Algo + Formula + Validate)
 		importCode = importAlgo + "\n" + importFormula + "\n" + importValidate;
@@ -167,53 +166,62 @@ public class MmlParsingJavaSkLearn {
 		return importCode;
 
 	}
-	
+
 	private String genBodypartForAlgorithm(MLAlgorithm algo) {
-		String algopart ="\n";
-		
+		String algopart = "\n";
+
 		if (algo instanceof DT) {
 			int maxd = ((DT) algo).getMax_depth();
 
 			algopart += "clf = ";
-			algopart += 
-				(maxd != 0) ? 
-				"tree.DecisionTreeClassifier(max_depth = " + maxd + ")\n" :
-				"tree.DecisionTreeClassifier()\n";
+			algopart += (maxd != 0) ? "tree.DecisionTreeClassifier(max_depth = " + maxd + ")\n"
+					: "tree.DecisionTreeClassifier()\n";
+		} else if (algo instanceof SVM) {
+
 		}
-		else if(algo instanceof SVM) {
-			
-		}
-		
+
 		return algopart;
 	}
-	
-	private String genBodypartForValidation(Validation validation){
+
+	private String genBodypartForValidation(Validation validation, MLAlgorithm algorithm, MMLModel model) {
 		String validationPart = "\n";
-		
+
 		// Training
 		StratificationMethod stratification = validation.getStratification();
 		if (stratification instanceof CrossValidation) {
-			validationPart += "";
-		} 
-		else if (stratification instanceof TrainingTest) {
+			if (algorithm instanceof DT) {
+				validationPart += "clf = DecisionTreeClassifier()\n" + "cross_val_score(clf, X, y, cv="
+						+ stratification.getNumber() + ")\n";
+			}
+			if (algorithm instanceof SVM) {
+
+			}
+			if (algorithm instanceof RandomForest) {
+
+			}
+			if (algorithm instanceof LogisticRegression) {
+
+			}
+			validationPart += "cross_validate()";
+		} else if (stratification instanceof TrainingTest) {
 			int test_size = 1 - stratification.getNumber() / 100;
 			validationPart += "test_size = " + test_size + "\n";
+			validationPart += "X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size)\n";
 
 		}
-		
+
 		return validationPart;
 	}
-	
+
 	private String genBodypartForPredictiveRFormula(RFormula formula) {
 		String rFormulaPart = "";
-		
-		if(formula.getPredictive() != null) {
-			
+
+		if (formula.getPredictive() != null) {
+
+		} else {
+
 		}
-		else {
-			
-		}
-		
+
 		return rFormulaPart;
 	}
 
