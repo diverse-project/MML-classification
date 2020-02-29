1b00c2f4145c6220df49da6b324ea4b7d3bc2880
Miguel Rivas <miguel.rivas@etudiant.univ-rennes1.fr>
2020-02-29 11:25:19 +0100

    commit

---
+++ org.xtext.example.mml.tests/src/org/xtext/example/mydsl/tests/groupeRivasSylla/compilers/EreCompiler.java
@@ -0,0 +1,10 @@
+package org.xtext.example.mydsl.tests.groupeRivasSylla.compilers;
+
+public class EreCompiler {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
